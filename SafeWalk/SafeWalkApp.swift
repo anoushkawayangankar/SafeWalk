4be5f95a48f244b634040757c9fa5b171579eb91
Anoushka <anoushkawayangankar@gmail.com>
2026-08-19 13:38:58 +0530

    Initial Commit

---
+++ SafeWalk/SafeWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafeWalkApp.swift
+//  SafeWalk
+//
+//  Created by Anoushka Wayangankar on 19/08/26.
+//
+
+import SwiftUI
+
+@main
+struct SafeWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
